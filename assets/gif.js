353719f0476becb0b081dfb68d1764ba673e951f
RANDOLF RESURRECCION <randolfresurreccion@RANDOLFs-MacBook-Pro.local>
2017-12-07 15:40:32 -0800

    added animate and still

--- assets/gif.js
+++ assets/gif.js
@@ -1,10 +1,27 @@
 
 $(document).ready(function () {
 
-    // renderbutton 
+    // pausing giff
+   
+
+    // function animate(gif) {
+        
+    //             if (gif.attr("src") === gif.attr("data-old")) {
+    //                 gif.attr("src", gif.attr("data-new"));
+    //             } else if (gif.attr("src") === gif.attr("data-new")) {
+    //                 gif.attr("src", gif.attr("data-old"));
+    //             } else {
+    //                 return
+    //             }
+    //         }
+    //     $(".gif").on("click", function(){
+    //         animate(gif);
+    //     });
+    
+   // renderbutton 
     function renderButton(input) {
         var newBtn = $("<button>");
-        newBtn.addClass("gif");
+        newBtn.addClass("button-gif");
         newBtn.text(input);
         newBtn.attr("data-name", input);
         $(".button-container").append(newBtn);
@@ -14,16 +31,24 @@ $(document).ready(function () {
     function runGifs(userInput) {
         var input = $(this).attr("data-name") || userInput;
         $("#gifs").empty();
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=H5GT7A551s0Eqb73TvpjlcWJhmeuCrp5&q=" + input + "&limit=10&offset=0&rating=G&lang=en"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=H5GT7A551s0Eqb73TvpjlcWJhmeuCrp5&q=" + input + "&limit=15&offset=0&rating=G&lang=en"
         $.ajax({
             url: queryURL,
             method: "get"
         }).done(function (response) {
             var data = response.data;
+            console.log(data);
             for (var i = 0; i < data.length; i++) {
                 var newImg = $("<img>");
-                newImg.attr("src", data[i].images.downsized.url);
+                newImg.attr("src", data[i].images.original.url);
+                newImg.attr("data-still", data[i].images.original_still.url);
+                
+                newImg.attr("data-animate", data[i].images.original.url);
+                newImg.attr("data-state", "still");
+               
+                newImg.addClass("imgClass");
                 $("#gifs").append(newImg);
+                
             }
         });
         $("#user-input").val("");
@@ -35,7 +60,19 @@ $(document).ready(function () {
         renderButton(userInput);
         runGifs(userInput);
     });
-    $(document).on("click", ".gif", runGifs);
+    $(document).on("click", ".button-gif", runGifs);
+    
+    $(document).on("click", ".imgClass", function() {
+        // $(".imgClass").on("click", function(){
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+              }
+        });
 
 });
 
